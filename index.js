43b30faf3eab3f9719643f684bb32ceaa024ef59
T-erry <terrywachuka10@gmail.com>
2022-12-06 13:51:33 +0300

    resubmitted the assignment

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-function fetchBooks() {
+
   // To pass the tests, don't forget to return your fetch
-  fetch("https://anapioficeandfire.com/api/books")
-  .then((resp) => resp.json())
-  .then((json) => renderBooks(json));
-}
-
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-    // console.log(main);
+  function fetchBooks() {
+    // To pass the tests, don't forget to return your fetch!
+    return fetch('https://anapioficeandfire.com/api/books')
+  
+    .then(res => res.json())
+    .then(books => renderBooks(books))
+  
+  
+  }
+  
+  function renderBooks(books) {
+    const main = document.querySelector('main');
+    books.forEach(book => {
+      const h2 = document.createElement('h2');
+      h2.innerHTML = book.name;
+      main.appendChild(h2);
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    fetchBooks();
   });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-   fetchBooks(); 
-});
